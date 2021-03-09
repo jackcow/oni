c52282d2c0135400c868e59e70643bf3b722aa1b
jackythecow <jackycow69@gmail.com>
2021-03-09 18:24:14 +0000

    added html and css to webserver

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import discord
 from discord.ext import commands
+from asyncio import sleep
 from replit import db
 from web_server import online
 
@@ -12,9 +13,18 @@ def get_prefix(client, message):
     db[message.guild.id] = '.'
 
   return db[message.guild.id]
+  
+  
+def get_num_members():
+  total = 0
+  for guild in client.guilds:
+    total += len(guild.members)
+    
+  return total
 
 
-client = commands.Bot(command_prefix=get_prefix)
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix=get_prefix,intents=intents)
 
 
 @client.event
@@ -38,17 +48,23 @@ async def on_command_error(ctx, error):
   await ctx.send(f"wtf bro?\n {error}")
 
 
+async def status_task():
+  while True:
+    await client.change_presence(activity=discord.Activity(type=0, name=f"in {len(db.keys())} Servers"))
+    await sleep(30)
+    await client.change_presence(activity=discord.Activity(type=0, name=f"with {len(set(client.get_all_members()))} Users"))
+    await sleep(30)
+
+
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
-  # await client.change_presence(activity=discord.Activity(type=0, name=f"in {len(db.keys())} Servers"))
-  await client.change_presence(activity=discord.Activity(type=0, name=f"with {len(set(client.get_all_members()))} Users"))
-
+  client.loop.create_task(status_task())
+  
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
-
 
 
 
--- web_server.py
+++ web_server.py
@@ -1,19 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 from threading import Thread
 import logging
 
 app = Flask('')
 
+# make ping log go away
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
+#PAGES
+# ---------------------------------------
+
 @app.route('/')
-def home():
-    return "six"
+def base():
+  return render_template('base.html')
+
+
+@app.route('/six')
+def six():
+  return 'six'
+
+# ---------------------------------------
 
 def run():
   app.run(host='0.0.0.0',port=8080)
 
 def online():
-    t = Thread(target=run)
-    t.start()
+  t = Thread(target=run)
+  t.start()
